279443d9869c12355d2faa9eeda226442f0ce2d5
Slaven Bacic <bacic.slaven@gmail.com>
2021-05-11 20:46:51 +0200

    chore(infrastructure): make language switchable and peristent

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,24 +1,27 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import detector from 'i18next-browser-languagedetector'
 import en from './locales/en'
 
 const resources = {
   en,
 }
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  saveMissing: true,
-  missingKeyHandler: (ng, ns, key) => {
-    if (process.env.NODE_ENV === 'development') {
-      console.warn(`Missing key: "${key}", ${ng}, ${ns}`)
-    }
-  },
-})
+i18n
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    saveMissing: true,
+    missingKeyHandler: (ng, ns, key) => {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn(`Missing key: "${key}", ${ng}, ${ns}`)
+      }
+    },
+  })
 
 export default i18n
